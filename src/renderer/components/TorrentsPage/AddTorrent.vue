88a788e01411e007d01d4fbc24118fa5a3fa04b5
Amandeep Panesar <baggordo@gmail.com>
2018-11-29 23:32:27 -0500

    Added UploadBox (CSS only)

--- src/renderer/components/TorrentsPage/AddTorrent.vue
+++ src/renderer/components/TorrentsPage/AddTorrent.vue
@@ -1,19 +1,58 @@
 <template>
-  <div>
-    Add Torrent Page
+  <div id="upload" align="center">
+    <h4>Add Torrent File:</h4>
+    <div class="dropbox">
+       <input type="file" class="input-file">
+       <p>Drag your files here or click to browse</p>
+    </div>
   </div>
 </template>
-
 <script>
   export default {
     data () {
       return {
-
+        stInital: 0,
+        stSaving: 1,
+        stSucess: 2,
+        stFailed: 3,
+        currentStatus: null
+      }
+    },
+    computed: {
+      isInitial () {
+        return this.currentStatus === this.stInital
+      },
+      isSaving () {
+        return this.currentStatus === this.stSaving
       }
     }
   }
 </script>
-
 <style scoped>
-  
+  .dropbox {
+    outline: 2px dashed grey; 
+    outline-offset: -10px;
+    background: lightcyan;
+    color: dimgray;
+    padding: 10px 10px;
+    min-height: 200px; 
+    position: relative;
+    cursor: pointer;
+  }
+  .input-file {
+    opacity: 0;
+    height: 100%;
+    width: 100%;
+    display: table-cell;
+    position: absolute;
+    cursor: pointer;
+  }
+  .dropbox p {
+    font-size: 1.2em;
+    text-align: center;
+    padding: 50px 0;
+  }
+  .dropbox:hover {
+    background: lightblue; /* when mouse over to the drop zone, change color */
+  }
 </style>
