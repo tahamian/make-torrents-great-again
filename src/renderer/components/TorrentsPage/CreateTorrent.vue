f6fd30dd6f809dcb62888821cf4bc952f54f0f8a
Amandeep Panesar <panesas2@mcmaster.ca>
2018-12-01 17:28:44 -0500

    List Files

--- src/renderer/components/TorrentsPage/CreateTorrent.vue
+++ src/renderer/components/TorrentsPage/CreateTorrent.vue
@@ -7,16 +7,12 @@
        <p>Drag your files here or click to browse</p>
     </div>
   </div>
-  <div id="table">
-    
-    
-    
-    
-  
-  
+  <div id="table" align = "center" class = "tcontainer" overflow-y>
+    <table id = "fileTable" align = "center" class = "table" style="width:100%">
+</table>
   </div>
-  <div id="createTorrent">
-    <button v-on:click="createTorrentFile()">Create Torrent</button>
+  <div id="createTorrent" align = "center">
+    <button  v-on:click="createTorrentFile()">Create Torrent</button>
   </div>
   </div>
 </template>
@@ -26,6 +22,7 @@
       return {
         obj: [],
         store: [],
+        rowId: 0,
         write: []
       }
     },
@@ -35,6 +32,7 @@
         console.log(input)
         for (var i = 0; i < input.length; i++) {
           this.obj.push({'name': input[i].name, 'path': input[i].path, 'category': this.checkFileType(input[i])})
+          this.tableCreate(input[i].name, input[i].path)
         }
         console.log(this.obj)
       },
@@ -57,20 +55,53 @@
           return 'other'
         }
       },
+      tableCreate (name, size) {
+        this.rowId++
+        // var thisRowID = this.rowId
+        var table = document.getElementById('fileTable')
+        var row = table.insertRow(0)
+        var cell0 = row.insertCell(0)
+        var cell1 = row.insertCell(1)
+        var cell2 = row.insertCell(2)
+        var btn = document.createElement('input')
+        btn.type = 'button'
+        btn.className = 'btn'
+        btn.onclick = function () {
+          alert('Baba')
+        }
+        // cell0.innerHTML = '<input type="button" value="x"></input>'
+        cell0.innerHTML = '<button class=\'btn\' v-on:click =this.buttonClicked()></button>'
+        cell1.innerHTML = name
+        cell2.innerHTML = size
+      },
+      buttonClicked () {
+        console.log('Worked')
+      },
+      tableReset () {
+        this.obj = []
+        this.write = []
+        var Table = document.getElementById('fileTable')
+        Table.innerHTML = ''
+      },
       handleFileUpload () {
         this.parse()
         // this.createTorrentFile(this.write)
+      },
+      deleteRow (rowid) {
+        var row = document.getElementById(rowid)
+        row.parentNode.removeChild(row)
       },
       createTorrentFile () {
         this.write = JSON.stringify(this.obj)
         console.log(this.write)
         const fs = require('fs')
         try {
-          fs.writeFileSync('torren.json', this.write, 'utf-8')
+          fs.writeFileSync('torrent.json', this.write, 'utf-8')
           console.log('File Written')
         } catch (e) {
           alert('Failed to save the file !')
         }
+        this.tableReset()
       }
     }
   }
@@ -85,6 +116,20 @@
     min-height: 200px; 
     position: relative;
     cursor: pointer;
+  }
+  .tcontainer{
+    position: relative;
+    padding: 10px 10px;
+    overflow: auto;
+    max-height: 25vh;
+    max-width: 100vh;
+  }
+  .table {
+    padding: 100px;
+  }
+  .smallB {
+   padding-top: 2px;
+   height: 50%;
   }
   .input-file {
     opacity: 0;
