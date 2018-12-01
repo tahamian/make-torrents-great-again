f037575170ce4ae0c798ca4baf603b5bcd3868fc
Victor A Velechovsky <velechva@mcmaster.ca>
2018-11-30 23:52:01 -0500

    Made UI layout less crap

---
+++ src/renderer/components/TorrentListHeader.vue
@@ -0,0 +1,31 @@
+<!-- Lord forgive me for my sins -->
+<template>
+<div class="torrent-list-header flex-row space-between">
+  <h1 style="min-width: 150px;">Image</h1>
+  <h1 style="width: 200px;">Name</h1>
+  <h1 style="width: 35px;">Download Speed</h1>
+  <h1 style="width: 35px;">Upload Speed</h1>
+  <h1 style="width: 35px;">Peers</h1>
+  <h1 style="width: 35px;">Delete Torrent</h1>
+  <h1 style="width: 35px;">More Options</h1>
+</div>
+</template>
+
+<script>
+</script>
+
+<style>
+.torrent-list-header
+{
+  align-items: center;
+}
+
+.torrent-list-header h1
+{
+  text-align: center;
+  margin-left: 20px;
+  margin-right: 20px;
+  font-size: 10px;
+  font-weight: bold;
+}
+</style>
